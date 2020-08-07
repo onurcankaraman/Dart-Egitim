b17a61d9c26fcd7153c59dc00b1abcd04f37b154
Onurcan KARAMAN <onurcankaraman340@gmail.com>
2020-08-07 07:42:55 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+	// Bankadan çekilen kredinin aylık geri ödeme tutarını ve yapılacak toplam geri ödeme tutarını hesaplayan programı yazalım. İlk olarak değişkenleri oluşturarak işe başlayalım.
+
+  double krediTutari = 4300.0;
+  double faizOrani = 1.25;
+  int taksitSayisi = 9;
+
+  double aylikOdeme = krediTutari / taksitSayisi; // kişinin faiz işlenmeden yapması gereken normal ödemeyi hesaplıyoruz.
+
+  aylikOdeme = aylikOdeme*faizOrani; // Aylik ödeme miktarina, aylık olarak ödemesi gereken faizi ekliyoruz.
+
+  print("Aylık olarak ödemeniz gereken taksit miktarı : $aylikOdeme TL.");
+  print("Geri Ödeyeceğiniz para miktarı :${aylikOdeme * taksitSayisi} TL"); // Eğer metinsel bir ifadenin içerisinde sayısal hesaplama yaptırıp yazma işlemi yapacaksak bunun için $ işaretinden sonra işlemleri {} içerisinde yaparız.
+
+}
